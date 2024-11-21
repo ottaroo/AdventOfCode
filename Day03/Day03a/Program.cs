28f2e1489e1d075f041f1a664268819214a847e7
Ottar Overskott Olsen <ottar@overskott.com>
2024-11-21 20:58:28 +0100

    Day03 - 2023

---
+++ Day03/Day03a/Program.cs
@@ -0,0 +1,100 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using System.Linq;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Attributes;
+
+namespace Day03a
+{
+
+    [MemoryDiagnoser]
+    public partial class Program
+    {
+        [GeneratedRegex(@"[^0123456789\.]", RegexOptions.IgnoreCase)]
+        private static partial Regex SchemaSymbols();
+
+        [GeneratedRegex(@"[0123456789]+", RegexOptions.IgnoreCase)]
+        private static partial Regex Numbers();
+
+
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Program>();
+            //ExecuteMain();
+        }
+
+        [Benchmark]
+        public void ExecuteMain()
+        {
+            using var fs = new FileStream(Path.Combine(AppContext.BaseDirectory, "input.txt"), FileMode.Open, FileAccess.Read);
+            using var sr = new StreamReader(fs);
+
+
+            string? previousLine = null;
+            string? nextLine = null;
+            var defaultColor = Console.ForegroundColor;
+
+            var sumOfPartNumbers = 0;
+
+            while (!sr.EndOfStream || nextLine != null)
+            {
+                var currentLine = nextLine ?? sr.ReadLine();
+                if (currentLine == null)
+                    continue;
+
+                if (!sr.EndOfStream)
+                    nextLine = sr.ReadLine();
+                else
+                    nextLine = null;
+
+
+
+                var numberMatches = Numbers().Matches(currentLine);
+                foreach (Match numberMatch in numberMatches)
+                {
+                    if (previousLine != null)
+                    {
+                        if (SearchForAdjacentSymbol(previousLine, numberMatch, ref sumOfPartNumbers))
+                            continue;
+                    }
+
+                    if (SearchForAdjacentSymbol(currentLine, numberMatch, ref sumOfPartNumbers))
+                        continue;
+
+                    if (nextLine != null)
+                    {
+                        if (SearchForAdjacentSymbol(nextLine, numberMatch, ref sumOfPartNumbers))
+                            continue;
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"[NO MATCH] {numberMatch.Value}");
+                    Console.ForegroundColor = defaultColor;
+                }
+
+                previousLine = currentLine;
+
+            }
+
+            Console.WriteLine($"Sum of Parts: {sumOfPartNumbers}");
+        }
+
+        private static bool SearchForAdjacentSymbol(ReadOnlySpan<char> checkLine, Match numberMatch, ref int sumOfParts)
+        {
+            foreach (var match in SchemaSymbols().EnumerateMatches(checkLine))
+            {
+                if (match.Index >= Math.Max(numberMatch.Index - 1, 0) && match.Index <= numberMatch.Index + numberMatch.Length)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("FOUND MATCH: ");
+                    Console.WriteLine($"Found symbol [{checkLine[match.Index]}] adjacent to number: {numberMatch.Value}");
+                    sumOfParts += int.Parse(numberMatch.Value);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+    }
+}
---
+++ Day03/Day03b/Program.cs
@@ -0,0 +1,142 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using System.Linq;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Attributes;
+using Microsoft.Diagnostics.Tracing.Parsers.ClrPrivate;
+
+namespace Day03b
+{
+
+    [MemoryDiagnoser]
+    public partial class Program
+    {
+        [GeneratedRegex(@"[*]", RegexOptions.IgnoreCase)]
+        private static partial Regex SchemaSymbols();
+
+        [GeneratedRegex(@"[0123456789]+", RegexOptions.IgnoreCase)]
+        private static partial Regex Numbers();
+
+
+        static void Main(string[] args)
+        {
+            //var summary = BenchmarkRunner.Run<Program>();
+            ExecuteMain();
+        }
+
+        [Benchmark]
+        public static void ExecuteMain()
+        {
+            using var fs = new FileStream(Path.Combine(AppContext.BaseDirectory, "input.txt"), FileMode.Open, FileAccess.Read);
+            using var sr = new StreamReader(fs);
+
+
+            string? previousLine = null;
+            string? nextLine = null;
+            var defaultColor = Console.ForegroundColor;
+
+            var sumOfPartNumbers = 0;
+
+            while (!sr.EndOfStream || nextLine != null)
+            {
+                var currentLine = nextLine ?? sr.ReadLine();
+                if (currentLine == null)
+                    continue;
+
+                if (!sr.EndOfStream)
+                    nextLine = sr.ReadLine();
+                else
+                    nextLine = null;
+
+
+
+                var gearMatches = SchemaSymbols().Matches(currentLine);
+                foreach (Match match in gearMatches)
+                {
+                    SearchForAdjacentNumbers(match.Index, previousLine, currentLine, nextLine, ref sumOfPartNumbers);
+
+                }
+
+                previousLine = currentLine;
+
+            }
+
+            Console.WriteLine($"Sum of Parts: {sumOfPartNumbers}");
+        }
+
+        private static bool SearchForAdjacentNumbers(int gearPosition, ReadOnlySpan<char> line, ref int sum, ref int count)
+        {
+            if (count > 2)
+                return false;
+
+            if (line.Length > 0)
+            {
+                foreach (Match number in Numbers().Matches(line.ToString()))
+                {
+                    if (count > 2)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"[False positive] More than two numbers connected to gear");
+                        return false;
+                    }
+
+                    if (gearPosition >= Math.Max(number.Index - 1, 0) && gearPosition <= number.Index + number.Length)
+                    {
+                        if (sum == 0)
+                            sum = int.Parse(number.Value);
+                        else
+                            sum *= int.Parse(number.Value);
+                        count++;
+                    }
+                }
+            }
+
+            return count <= 2;
+        }
+
+        private static bool SearchForAdjacentNumbers(int gearPosition, ReadOnlySpan<char> previous, ReadOnlySpan<char> current, ReadOnlySpan<char> next, ref int sumOfParts)
+        {
+
+            var partSum = 0;
+            var numberOfMatches = 0;
+
+            if (previous.Length > 0)
+            {
+                if (!SearchForAdjacentNumbers(gearPosition, previous, ref partSum, ref numberOfMatches))
+                    goto FAILED_TO_FIND_OR_FOUND_TO_MANY;
+            }
+            if (current.Length > 0)
+            {
+                if (!SearchForAdjacentNumbers(gearPosition, current, ref partSum, ref numberOfMatches))
+                    goto FAILED_TO_FIND_OR_FOUND_TO_MANY;
+            }
+            if (next.Length > 0)
+            {
+                if (!SearchForAdjacentNumbers(gearPosition, next, ref partSum, ref numberOfMatches))
+                    goto FAILED_TO_FIND_OR_FOUND_TO_MANY;
+            }
+
+
+            if (numberOfMatches == 2)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"[MATCH] Gear position: {gearPosition}");
+                sumOfParts += partSum;
+                return true;
+            }
+
+
+        FAILED_TO_FIND_OR_FOUND_TO_MANY: 
+            Console.ForegroundColor = ConsoleColor.Red;
+            if (numberOfMatches < 2)
+                Console.WriteLine($"[NO MATCH] Gear position: {gearPosition}");
+            else
+                Console.WriteLine($"[False positive] More than two numbers connected to gear");
+
+            return false;
+        }
+
+
+
+    }
+}
