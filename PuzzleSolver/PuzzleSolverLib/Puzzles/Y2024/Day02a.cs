1432ebab5afe786f17c8add2a3e900da0efd228f
Ottar Overskott Olsen <ottar@overskott.com>
2024-12-02 18:10:49 +0100

    2024 Day 2

---
+++ PuzzleSolver/PuzzleSolverLib/Puzzles/Y2024/Day02a.cs
@@ -0,0 +1,60 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace PuzzleSolverLib.Puzzles.Y2024;
+
+public partial class Day02a : PuzzleBaseClass
+{
+
+    [GeneratedRegex("\\d+")]
+    public partial Regex Numbers();
+
+
+    public bool IsSafeReport(ReadOnlySpan<int> numbers)
+    {
+        var increasing = 0;
+        for (var n = 1; n < numbers.Length; n++)
+        {
+            if (increasing == 0)
+                increasing = numbers[n] > numbers[n - 1] ? 1 : -1;
+
+            if (Math.Abs(numbers[n] - numbers[n - 1]) > 3 || numbers[n] - numbers[n-1] == 0 || (numbers[n] - numbers[n - 1]) * increasing < 0)
+                return false;
+
+        }
+
+        return true;
+    }
+
+
+    public override string? OnSolve(ReadOnlySpan<char> inputFile)
+    {
+        try
+        {
+            var safeReports = 0;
+            using var fs = new FileStream(inputFile.ToString(), FileMode.Open, FileAccess.Read, FileShare.Read);
+            using var sr = new StreamReader(fs);
+
+            while (!sr.EndOfStream)
+            {
+                var line = sr.ReadLine();
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                var matches = Numbers().Matches(line);
+                if (!matches.Any()) continue;
+
+                var numbers = matches.Select(x => int.Parse(x.Value)).ToArray();
+
+                if (IsSafeReport(numbers))
+                    safeReports++;
+            }
+
+            return safeReports.ToString();
+        }
+        catch (Exception ex)
+        {
+            LastError = ex;
+            return null;
+        }
+
+    }
+}
---
+++ PuzzleSolver/PuzzleSolverLib/Puzzles/Y2024/Day02b.cs
@@ -0,0 +1,89 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PuzzleSolverLib.Puzzles.Y2024;
+
+public partial class Day02b : PuzzleBaseClass
+{
+
+    [GeneratedRegex("\\d+")]
+    public partial Regex Numbers();
+
+
+    private int[] GetDiff(ReadOnlySpan<int> numbers)
+    {
+        var diffs = new int[numbers.Length - 1];
+        for (var n = 1; n < numbers.Length; n++)
+        {
+            diffs[n - 1] = numbers[n] - numbers[n - 1];
+        }
+        return diffs;
+    }
+
+    private bool IsSafelyIncreasingOrDecreasing(ReadOnlySpan<int> numbers)
+    {
+        var diff = GetDiff(numbers);
+        if (diff.All(x=> x > 0 && x <= 3) || diff.All(x=>x < 0 && x >= -3))
+            return true;
+        return false;
+    }
+
+    public bool IsSafeReport(ReadOnlySpan<int> numbers)
+    {
+        if (IsSafelyIncreasingOrDecreasing(numbers))
+            return true;
+
+        var tmp = new int[numbers.Length - 1];
+
+        // We accept one error - test if we have a good report if we skip one number
+        var indexToSkip = 0;
+        for (var i = 0; i < numbers.Length; i++)
+        {
+            for (var n = 0; n < numbers.Length; n++)
+            {
+                if (indexToSkip == n) continue;
+
+                tmp[n > indexToSkip ? n - 1 : n] = numbers[n];
+            }
+            indexToSkip++;
+            if (IsSafelyIncreasingOrDecreasing(tmp))
+                return true;
+        }
+        return false;
+    }
+
+
+    public override string? OnSolve(ReadOnlySpan<char> inputFile)
+    {
+        try
+        {
+            var safeReports = 0;
+            using var fs = new FileStream(inputFile.ToString(), FileMode.Open, FileAccess.Read, FileShare.Read);
+            using var sr = new StreamReader(fs);
+
+            var row = 0;
+            while (!sr.EndOfStream)
+            {
+                var line = sr.ReadLine();
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                var matches = Numbers().Matches(line);
+                if (!matches.Any()) continue;
+
+                var numbers = matches.Select(x => int.Parse(x.Value)).ToArray();
+
+
+                if (IsSafeReport(numbers))
+                    safeReports++;
+
+                row++;
+            }
+
+            return safeReports.ToString();
+        }
+        catch (Exception ex)
+        {
+            LastError = ex;
+            return null;
+        }
+
+    }
+}
