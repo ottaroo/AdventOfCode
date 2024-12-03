be4d6780ab709a81ab7be8b95bca0386f30b5ced
Ottar Overskott Olsen <ottar@overskott.com>
2024-12-03 15:14:17 +0100

    2024 Day 3

---
+++ PuzzleSolver/PuzzleSolverLib/Puzzles/Y2024/Day03a.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PuzzleSolverLib.Puzzles.Y2024
+{
+    public partial class Day03a : PuzzleBaseClass
+    {
+
+        [GeneratedRegex(@"mul\((?<left>\d+),(?<right>\d+)\)")]
+        public partial Regex MulPattern();
+
+        public override string? OnSolve(ReadOnlySpan<char> inputFile)
+        {
+            try
+            {
+                var corruptedFile = File.ReadAllText(inputFile.ToString());
+                var corruptedFileSpan = corruptedFile.AsSpan();
+                var sum = 0;
+                
+                foreach (ValueMatch valueMatch in MulPattern().EnumerateMatches(corruptedFileSpan))
+                {
+                    var txt = corruptedFileSpan.Slice(valueMatch.Index, valueMatch.Length);
+                    var match = MulPattern().Match(txt.ToString());
+                    var left = int.Parse(match.Groups["left"].Value);
+                    var right = int.Parse(match.Groups["right"].Value);
+                    sum += (left * right);
+                }
+
+                return sum.ToString();
+            }
+            catch (Exception ex)
+            {
+                LastError = ex;
+                return null;
+            }
+        }
+    }
+}
---
+++ PuzzleSolver/PuzzleSolverLib/Puzzles/Y2024/Day03b.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PuzzleSolverLib.Puzzles.Y2024
+{
+    public partial class Day03b : PuzzleBaseClass
+    {
+
+        [GeneratedRegex(@"(?<do>don't\(\)|do\(\))|mul\((?<left>\d+),(?<right>\d+)\)")]
+        public partial Regex MulPattern();
+
+        public override string? OnSolve(ReadOnlySpan<char> inputFile)
+        {
+            try
+            {
+                var corruptedFile = File.ReadAllText(inputFile.ToString());
+                var corruptedFileSpan = corruptedFile.AsSpan();
+                var sum = 0;
+                var doMultiplication = true;
+                foreach (ValueMatch valueMatch in MulPattern().EnumerateMatches(corruptedFileSpan))
+                {
+                    var txt = corruptedFileSpan.Slice(valueMatch.Index, valueMatch.Length);
+                    
+                    var match = MulPattern().Match(txt.ToString());
+
+                    if(match.Groups["do"].Success)
+                    {
+                        doMultiplication = match.Groups["do"].Value.Equals("do()");
+                        continue;
+                    }
+
+                    if (!doMultiplication)
+                        continue;
+
+                    var left = int.Parse(match.Groups["left"].Value);
+                    var right = int.Parse(match.Groups["right"].Value);
+                    sum += (left * right);
+                }
+
+                return sum.ToString();
+            }
+            catch (Exception ex)
+            {
+                LastError = ex;
+                return null;
+            }
+        }
+    }
+}
